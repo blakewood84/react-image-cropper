032d37d77ea98f15c5e58f46a0c1954b56599b55
Blake Wood Jr <blake@rewired.one>
2022-06-28 08:47:07 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
